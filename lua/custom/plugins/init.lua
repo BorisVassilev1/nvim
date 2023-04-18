743bd3764cf5cdf806c74dd17a27d4504129ce31
Boris Vassilev <boris.n.vassilev@gmail.com>
2023-04-18 23:15:03 +0300

    Initial Commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,17 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+	{'neoclide/coc.nvim', branch = 'release'},
+	'tikhomirov/vim-glsl',
+	{"nvim-tree/nvim-tree.lua",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {}
+		end,
+	}
+}
