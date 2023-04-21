f37411e297dd0a5d700152bbe137ef93ebb542a8
Boris Vassilev <boris.n.vassilev@gmail.com>
2023-04-21 12:02:30 +0300

    coc was not loaded?

--- init.lua
+++ init.lua
@@ -236,7 +236,7 @@ vim.keymap.set({'n', 'v', 'i'}, '<A-l>', '<C-w>l');
 vim.keymap.set({'n', 'v', 'i'}, '<A-j>', '<C-w>j');
 vim.keymap.set({'n', 'v', 'i'}, '<A-k>', '<C-w>k');
 
-vim.keymap.set({'n', 'v', 'i'}, '<M-i>', ':NvimTreeToggle<CR>');
+vim.keymap.set({'n', 'v', 'i'}, '<M-i>', ':NvimTreeToggle<CR>', { silent = true });
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -358,6 +358,8 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+require('plugins/coc_setup')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
